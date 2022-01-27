6581778acee39daf312b35120888667026ea9937
Almaz <Almazbek.sheraliev321@gmail.com>
2022-01-27 17:08:32 +0600

    added store

--- src/api/api.js
+++
@@ -1,2 +0,0 @@
-const API_URL = 'http://localhost:8080/api/auth/'
-export default API_URL
--- src/containers/LoginPage.js
+++ src/containers/LoginPage.js
@@ -1,61 +1,12 @@
 import React from 'react'
-import * as Yup from 'yup'
-import { Box, Button, LinearProgress } from '@mui/material'
-import { useFormik } from 'formik'
-import { useDispatch, useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import { Box, Button } from '@mui/material'
 import loginImg from '../assets/images/loginImage.png'
 import classes from '../assets/styles/LoginPage.module.css'
 import Input from '../components/UI/Input'
-import { login } from '../store/authSlice'
 
 export default function LoginPage() {
-   const dispatch = useDispatch()
-
-   const navigate = useNavigate()
-
-   const { isLoggedIn } = useSelector((state) => state.auth)
-
-   const [isLoading, setIsloading] = React.useState(false)
-
-   const handleLogin = (formValue) => {
-      const { email, password } = formValue
-      dispatch(login({ email, password }))
-      setIsloading(true)
-      return navigate('/admin')
-      // ...
-   }
-
-   //* validation
-   const validationSchema = Yup.object({
-      email: Yup.string('Enter your email')
-         .required('Email is required!')
-         .email('enter a valid email'),
-      password: Yup.string('Enter your password')
-         .min(8, 'Password should be of minimum 8 characters length')
-         .required('Password is required'),
-   })
-
-   const formik = useFormik({
-      initialValues: {
-         email: '',
-         password: '',
-      },
-      validationSchema,
-      onSubmit: handleLogin,
-   })
-
-   if (isLoggedIn) return navigate('/admin')
-
    return (
-      <Box className={classes.container} sx={{ opacity: isLoading ? 0.7 : '' }}>
-         <LinearProgress
-            sx={{
-               width: '100%',
-               flex: '0 0 100%',
-               display: isLoading ? 'block' : 'none',
-            }}
-         />
+      <Box className={classes.container}>
          <div className={classes.leftSide}>
             <img className={classes.loginImg} src={loginImg} alt="loginImage" />
          </div>
@@ -64,29 +15,14 @@ export default function LoginPage() {
             <h1 className={classes.loginTitle}>
                Добро пожаловать <br /> в <span>Peaksoft LMS !</span>
             </h1>
-            <form className={classes.loginForm} onSubmit={formik.handleSubmit}>
+            <form className={classes.loginForm}>
                <label htmlFor="email">Логин:</label>
-               <Input
-                  id="email"
-                  name="email"
-                  placeholder="Введите email"
-                  value={formik.values.email}
-                  onChange={formik.handleChange}
-                  error={formik.touched.email && Boolean(formik.errors.email)}
-                  helperText={formik.touched.email && formik.errors.email}
-               />
+               <Input id="email" name="email" placeholder="Введите email" />
                <label htmlFor="password">Пароль:</label>
                <Input
                   placeholder="Введите пароль"
                   name="password"
                   id="password"
-                  type="password"
-                  value={formik.values.password}
-                  onChange={formik.handleChange}
-                  error={
-                     formik.touched.password && Boolean(formik.errors.password)
-                  }
-                  helperText={formik.touched.password && formik.errors.password}
                />
                <Button sx={{ fontSize: 18 }} variant="contained" type="submit">
                   Войти
@@ -96,5 +32,3 @@ export default function LoginPage() {
       </Box>
    )
 }
-
-// img src=asdklflasdf
