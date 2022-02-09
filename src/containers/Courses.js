6d60217c9b57e61d9337767d5994795ebfbdc1b9
AlmazHecker <almazbek.sheraliev321@gmail.com>
2022-02-09 14:10:58 +0600

    fixed

--- src/containers/Courses.js
+++ src/containers/Courses.js
@@ -7,33 +7,34 @@ import CustomizedCard from '../components/UI/Card/CustomizedCard'
 import Flexer from '../components/UI/Flexer'
 import Table from '../components/UI/Table'
 
-const DefaultRoute = ({ data, isLoading }) => (
-   <Flexer>
-      {data.map((item) =>
-         isLoading ? (
-            <Stack key={item.id} spacing={1} m="10px" height="250px">
-               <Skeleton variant="rectangular" width={250} height={140} />
-               <Skeleton variant="text" width="60%" height="auto" />
-               <Skeleton animation="wave" height={10} sx={{ mb: 1 }} />
-               <Skeleton animation="wave" height={10} />
-               <Skeleton animation="wave" height={10} width="80%" />
-            </Stack>
-         ) : (
-            <CustomizedCard {...item} key={item.id} />
-         )
-      )}
-   </Flexer>
-)
+const CourseRoute = ({ data }) => {
+   const isLoading = useSelector((state) => state.data.loading)
 
-export default function Groups() {
-   const loading = useSelector((state) => state.data.loading)
+   return (
+      <Flexer>
+         {data.map((item) =>
+            isLoading ? (
+               <Stack key={item.id} spacing={1} m="10px" height="250px">
+                  <Skeleton variant="rectangular" width={250} height={140} />
+                  <Skeleton variant="text" width="60%" height="auto" />
+                  <Skeleton animation="wave" height={10} sx={{ mb: 1 }} />
+                  <Skeleton animation="wave" height={10} />
+                  <Skeleton animation="wave" height={10} width="80%" />
+               </Stack>
+            ) : (
+               <CustomizedCard {...item} key={item.id} />
+            )
+         )}
+      </Flexer>
+   )
+}
+
+export default function Courses() {
    const data = useSelector((state) => state.data.data)
+
    return (
       <Routes>
-         <Route
-            path="/"
-            element={<DefaultRoute data={data} isLoading={loading} />}
-         />
+         <Route path="/" element={<CourseRoute data={data} />} />
          <Route path="/:courseId" element={<Table />} />
       </Routes>
    )
